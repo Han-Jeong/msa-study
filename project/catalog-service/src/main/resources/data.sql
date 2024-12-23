8cf55cd5aaa095f37a02e4a9cc3311e4171aa809
hann <wjdgks1233@gmail.com>
2024-12-23 23:51:20 +0900

    section/6 : complete setting of catalog-service #5

---
+++ project/catalog-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO catalog (product_id, product_name, stock, unit_price)
+VALUES ('CATALOG-001', 'Berlin', 100, 1500);
+
+INSERT INTO catalog (product_id, product_name, stock, unit_price)
+VALUES ('CATALOG-002', 'Tokyo', 110, 1000);
+
+INSERT INTO catalog (product_id, product_name, stock, unit_price)
+VALUES ('CATALOG-003', 'StockHolm', 120, 2000);
