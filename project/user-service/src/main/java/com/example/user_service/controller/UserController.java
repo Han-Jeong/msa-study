e480e362e29f8eafff6daeaa8f25557cc5169661
hann <wjdgks1233@gmail.com>
2024-12-23 22:00:48 +0900

    section/6 : add user service(get List of user & get user) #5

--- project/user-service/src/main/java/com/example/user_service/controller/UserController.java
+++ project/user-service/src/main/java/com/example/user_service/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.user_service.controller;
 
 import com.example.user_service.dto.UserDto;
+import com.example.user_service.jpa.UserEntity;
 import com.example.user_service.service.UserService;
 import com.example.user_service.vo.Greeting;
 import com.example.user_service.vo.RequestUser;
@@ -13,8 +14,12 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.modelmapper.convention.MatchingStrategies.STRICT;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @Slf4j
 @RestController
@@ -32,11 +37,13 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("health_check")
+    @GetMapping("/health_check")
     public String status() {
-        return "It's working in UserService";
+        return String.format("It's working in UserService on Port %s",
+                env.getProperty("local.server.port"));
     }
-    @GetMapping("welcome")
+
+    @GetMapping("/welcome")
     public String welcome() {
 //        return env.getProperty("greeting.message");
         return greeting.getMessage();
@@ -50,5 +57,22 @@ public class UserController {
         UserDto responseDto = userService.createUser(userDto);
         ResponseUser responseUser = mapper.map(responseDto, ResponseUser.class);
         return ResponseEntity.status(CREATED).body(responseUser);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<ResponseUser>> getUsers() {
+        Iterable<UserEntity> usersByAll = userService.getUsersByAll();
+        List<ResponseUser> result = new ArrayList<>();
+        usersByAll.forEach(v -> {
+            result.add(new ModelMapper().map(v, ResponseUser.class));
+        });
+        return ResponseEntity.status(OK).body(result);
+    }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<ResponseUser> getUserById(@PathVariable String userId) {
+        UserDto userDto = userService.getUserByUserId(userId);
+        ResponseUser result = new ModelMapper().map(userDto, ResponseUser.class);
+        return ResponseEntity.status(OK).body(result);
     }
 }
--- project/user-service/src/main/java/com/example/user_service/service/UserServiceImpl.java
+++ project/user-service/src/main/java/com/example/user_service/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.user_service.service;
 import com.example.user_service.dto.UserDto;
 import com.example.user_service.jpa.UserEntity;
 import com.example.user_service.jpa.UserRepository;
+import com.example.user_service.vo.ResponseOrder;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
@@ -10,6 +11,8 @@ import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static org.modelmapper.convention.MatchingStrategies.*;
@@ -31,5 +34,22 @@ public class UserServiceImpl implements UserService{
 
         userRepository.save(user);
         return modelMapper.map(user, UserDto.class);
+    }
+
+    @Override
+    public UserDto getUserByUserId(String userId) {
+        UserEntity user = userRepository.findByUserId(userId);
+        if (user == null) {
+            throw new RuntimeException("user not found");
+        }
+        UserDto userDto = new ModelMapper().map(user, UserDto.class);
+        List<ResponseOrder> orders = new ArrayList<>();
+        userDto.setOrders(orders);
+        return userDto;
+    }
+
+    @Override
+    public Iterable<UserEntity> getUsersByAll() {
+        return userRepository.findAll();
     }
 }
